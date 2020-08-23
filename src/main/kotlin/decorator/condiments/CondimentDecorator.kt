87bac483d41159f900b33cd6facaa9945b9d295e
Jason Adam <22adamj22@gmail.com>
2020-08-22 21:52:23 -0700

    fixed decorator

--- src/main/kotlin/decorator/condiments/CondimentDecorator.kt
+++ src/main/kotlin/decorator/condiments/CondimentDecorator.kt
@@ -3,7 +3,4 @@ package decorator.condiments
 import decorator.beverages.Beverage
 
 abstract class CondimentDecorator : Beverage() {
-    override fun cost(): Double {
-        TODO("Not yet implemented")
-    }
 }
